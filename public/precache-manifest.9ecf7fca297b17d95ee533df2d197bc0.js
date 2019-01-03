24a1569f475c53295ca3e263588b1bfb5a1cd789
adam.html <adam.html@kakaomobility.com>
2019-01-03 10:48:15 +0900

    heroku setting

---
+++ public/precache-manifest.9ecf7fca297b17d95ee533df2d197bc0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2466d39bf742c5bceed",
+    "url": "/static/js/main.b2466d39.chunk.js"
+  },
+  {
+    "revision": "ae3110a79aa58930e15d",
+    "url": "/static/js/1.ae3110a7.chunk.js"
+  },
+  {
+    "revision": "986b8a3fa461cb30825845907ef1c405",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,13 +16,11 @@ app.use('/graphql',
   })
 );
 
-/*
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
-*/
 
 const PORT = process.env.PORT || 5000;
 
